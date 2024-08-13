6edadba0b6e3d8459896709ce2e064c06d20c77e
Sigmund Frost <sigmafi.tech@gmail.com>
2024-08-13 07:30:19 +0200

    Styled 404

--- oot-monster-codex-next/app/not-found.tsx
+++ oot-monster-codex-next/app/not-found.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+'use client'
+import React, { useState, useEffect } from 'react'
+import HeaderNoSearch from './_components/headernosearch'
+import Footer from './_components/footer'
+import InfoCard from './_components/infocard'
+
+interface Card {
+  card: {
+    id: string
+    name: string
+    description: string
+    boss_text?: string
+    navi_text?: string
+    hp: number
+    damage: number
+    dot: number
+    weakness: string
+    strength: string
+    image_url: string
+  }
+  details?: {
+    item?: string
+    amount?: string
+    rate?: number
+  }[]
+}
 
 const PageNotFound = () => {
+
+  const card = {
+    card: {
+      id: '404',
+      name: 'Page Not Found',
+      description: 'The page you requested could not be found.',
+      boss_text: '',
+      navi_text: 'Hey, listen! This page does not exist.',
+      hp: 0,
+      damage: 0,
+      dot: 0,
+      weakness: '',
+      strength: '',
+      image_url: ''
+    },
+    details: []
+  }
+
   return (
-    <div>404</div>
+    <div className='min-h-screen flex flex-col justify-between text-[#E0E0E0]'>
+      <HeaderNoSearch />
+      <InfoCard card={card?.card} details={card?.details}/>
+      <Footer />
+    </div>
   )
 }
 
